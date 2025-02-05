f0714fa712a61d54ea945800482bbe93d87c840a
Daniel Dilts <diltsman@gmail.com>
2025-02-04 21:46:44 -0700

    Run clang-format on main

--- src/elf/main.cpp
+++ src/elf/main.cpp
@@ -1,2 +1,6 @@
 export module main;
-auto main(int argc, char *argv[]) -> int { static_cast<void>(argc); static_cast<void>(argv); return 0; }
+auto main(int argc, char *argv[]) -> int {
+  static_cast<void>(argc);
+  static_cast<void>(argv);
+  return 0;
+}
